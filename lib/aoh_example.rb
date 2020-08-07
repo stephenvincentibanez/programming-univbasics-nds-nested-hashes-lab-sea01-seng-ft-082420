6d36b1f2d3bde3037fd91ae058f7ba17ea755a8e
Stephen Ibanez <stephenvincentibanez@gmail.com>
2020-08-07 17:57:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,4 +29,7 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+   character_array =
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  character_array[1][:name]="Joelle van Dyne (PGOAT)"
 end
